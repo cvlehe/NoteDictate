c0e0fbe68ca34afa125e2098e8b8c874c65a07c5
Charlie von Lehe <vonlehec@gmail.com>
2019-03-12 00:28:54 -0400

    Added table and gitignore file

--- NoteDictate WatchKit Extension/InterfaceController.swift
+++ NoteDictate WatchKit Extension/InterfaceController.swift
@@ -11,11 +11,18 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet var table:WKInterfaceTable!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        
+        table.setNumberOfRows(10, withRowType: "Row")
+        for rowIndex in 0..<10 {
+            guard let row = table.rowController(at: rowIndex) as? NoteSelectRow else { continue }
+            row.textLabel.setText("Row \(rowIndex)")
+        }
     }
     
     override func willActivate() {
@@ -26,6 +33,10 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    @IBAction func addNewNote() {
+        
     }
 
 }
---
+++ NoteDictate WatchKit Extension/NoteSelectRow.swift
@@ -0,0 +1,13 @@
+//
+//  NoteSelectRow.swift
+//  NoteDictate WatchKit Extension
+//
+//  Created by Charlie von Lehe on 3/12/19.
+//  Copyright © 2019 Charlie von Lehe. All rights reserved.
+//
+
+import WatchKit
+
+class NoteSelectRow: NSObject {
+    @IBOutlet var textLabel: WKInterfaceLabel!
+}
